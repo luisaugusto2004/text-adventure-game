b952651a365926bde824634f2b7fd3a3d8d0d281
Luis Augusto <luisaugusto010203@gmail.com>
2025-04-28 12:50:36 -0300

    feat: refactoring TextPrinter to permit skipping animation

--- text-adventure-game/RPGTextGame/Util/TextPrinter.cs
+++ text-adventure-game/RPGTextGame/Util/TextPrinter.cs
@@ -10,10 +10,18 @@ namespace Util
     {
         public static void Print(string text, int speed)
         {
-            foreach (char c in text)
+
+            for (int i = 0; i < text.Length; i++)
             {
-                Console.Write(c);
-                System.Threading.Thread.Sleep(speed);
+                Console.Write(text[i]);
+
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+                    Console.Write(text.Substring(i + 1));
+                    break;
+                }
+                Thread.Sleep(speed);
             }
             Console.WriteLine();
         }
