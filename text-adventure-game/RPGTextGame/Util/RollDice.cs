1471a79bc18bbef6f3e25846a3eb8251034d80d4
Luis Augusto <luisaugusto010203@gmail.com>
2025-05-07 20:01:23 -0300

    feat: add RollDice utility for consistent random value generation

---
+++ text-adventure-game/RPGTextGame/Util/RollDice.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System;
+
+namespace Util
+{
+    class RollDice
+    {
+        public static int Roll(int faces, int rolls)
+        {
+            List<int> values = new List<int>(); 
+            for (int i = 0; i < rolls; i++)
+            {
+                int dice_roll = Game.GlobalRandom.Next(1, faces + 1);
+                values.Add(dice_roll);
+            }
+
+            return values.Sum();  
+        }
+    }
+}
