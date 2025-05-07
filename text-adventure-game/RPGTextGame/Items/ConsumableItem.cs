9a7d004d08f70f26e3dea466d9e2722721548a7d
Luis Augusto <luisaugusto010203@gmail.com>
2025-05-07 19:16:31 -0300

    feat: adding consumableItem class

---
+++ text-adventure-game/RPGTextGame/Items/ConsumableItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Items
+{
+    class ConsumableItem : Item
+    {
+        private int healthGain;
+
+        public ConsumableItem(string name, string description, int _healthGain) : base(name, description) 
+        {
+            healthGain = _healthGain;  
+        }
+    }
+}
